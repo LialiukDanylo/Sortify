1831d07068bfc5a198af3ac20dff4a0ca0fc4730
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-05-31 03:37:52 +0200

    feat: add user input handling

--- Sortify/Program.cs
+++ Sortify/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Sortify
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please, enter the folder path");
+            while (true)
+            {
+                Console.Write("Path: ");
+                string path = Console.ReadLine();
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine($"Selected path: {path}");
+                }
+                else
+                {
+                    Console.WriteLine("Folder does not exist. Try again.");
+                }
+            }
+        }
+    }
+}
